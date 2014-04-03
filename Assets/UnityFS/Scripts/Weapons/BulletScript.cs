434eafdf6d5002a3181d9a882f668ac588d3adb1
unknown <gundermanc@gmail.com>
2014-04-03 16:02:24 -0400

    Implemented lame bullets. Signed-off-by: unknown <gundermanc@gmail.com>

---
+++ Assets/UnityFS/Scripts/Weapons/BulletScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Bullet Script
+ * @author Christian Gunderman
+ */
+public class BulletScript : MonoBehaviour {
+	private Vector3 startPosition;
+
+	// Use this for initialization
+	void Start () {
+		this.rigidbody.velocity = Vector3.forward * 10;
+		this.startPosition = this.rigidbody.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Vector3.Distance(startPosition, this.rigidbody.position) > 10) {
+			//Destroy(this.gameObject);
+		}
+	}
+
+	/**
+	 * Destroy this bullet
+	 */
+	void OnTriggerEnter(Collider other) {
+		//Destroy(this.gameObject);
+	}
+}
---
+++ Assets/UnityFS/Scripts/Weapons/WeaponScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponScript : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)) {
+			Instantiate(bulletPrefab, this.transform.position + new Vector3(0f, 1, 5f), this.transform.rotation);
+		}
+	}
+}
