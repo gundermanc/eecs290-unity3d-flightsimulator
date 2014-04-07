ee5d82f3ccf4e3e903c47d69b4cb035443e19bda
John Billingsley <jmb330@case.edu>
2014-04-07 13:18:08 -0400

    added the networking script but its not working yet, maybe someone could look at it, the player spawns then immediataly disspapears

---
+++ Assets/NetworkingScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkingScript : Photon.MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.ConnectUsingSettings("alpha 0.1");
+	}
+
+	void OnGUI(){
+		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+	}
+
+	void OnJoinedLobby(){
+		PhotonNetwork.JoinRandomRoom();
+	}
+
+	void OnRandomRoomJoinFailed(){
+		PhotonNetwork.CreateRoom(null);
+	}
+
+	void OnJoinedRoom(){
+		Vector3 spawnPoint = new Vector3(0f, 1000f, 0f);
+		GameObject myShip = PhotonNetwork.Instantiate("Ship1", spawnPoint, Quaternion.identity, 0);
+		myShip.transform.parent = GameObject.Find("Players").transform;
+	}
+}
