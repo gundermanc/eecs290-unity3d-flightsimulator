5b2326bf0e92e4a1c2642b4a8780789911e66165
masroorq17 <masroorq@gmail.com>
2014-04-08 14:39:55 -0400

    Hit enter on numpad to reset plane if it gets stuck

---
+++ Assets/UnityFS/Scripts/PlaneReset.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneReset : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.KeypadEnter)) {
+			Reset();
+		}
+	}
+
+	//Just resets the aircraft position in case you get stuck
+	void Reset () {
+		Vector3 resetPoint = new Vector3(0f, 1000f, 0f);
+		this.transform.position = resetPoint;
+	}
+}
